aee0c683ed6914a86ad0f2b015c9535cc8c98542
Michael Dwan <m@dwan.io>
2020-12-01 13:31:51 -0700

    configure maxmemory to 90% of FLY_VM_MEMORY_MB

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+MAXMEMORY=${FLY_VM_MEMORY_MB:-512}
+MAXMEMORY="$(((($FLY_VM_MEMORY_MB*10)-$FLY_VM_MEMORY_MB)/10))"
+
 sysctl vm.overcommit_memory=1
 sysctl net.core.somaxconn=1024
-redis-server --requirepass $REDIS_PASSWORD --bind 0.0.0.0 --dir /data/ --save "" --maxmemory-policy allkeys-lfu --maxmemory 500mb
+redis-server --requirepass $REDIS_PASSWORD --bind 0.0.0.0 --dir /data/ --save "" --maxmemory-policy allkeys-lfu --maxmemory "${MAXMEMORY}mb"
